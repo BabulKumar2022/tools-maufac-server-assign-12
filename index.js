7dd5c6be1bed0c3baa9322214d3763caded68d96
BabulKumar2022 <bb3583400@gmail.com>
2022-05-25 21:41:28 +0600

    user api done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,22 +21,74 @@ async function run(){
         // console.log('Database is connected');
         const serviceCollection = client.db('tool_trade').collection('electric_tools');
         const bookingCollection = client.db('tool_trade').collection('bookings');
-       
+        const userCollection = client.db('tool_trade').collection('users');
 
-        app.get('/service', async(req, res) =>{
-            const query ={};
-            const cursor = serviceCollection.find(query);
-            const services =await cursor.toArray();
-            res.send(services);
+        //admin auth
+        app.get('/admin/:email', async(req, res)=>{
+          const email =req.params.email;
+          const user = await userCollection.findOne({email:email});
+          const isAdmin = user.role === 'admin';
+          res.send({admin: isAdmin});
         })
 
-//from modal to db
 
-app.post('/booking', async(req, res)=>{
-  const booking = req.body;
-  const result =await bookingCollection.insertOne(booking);
-  return  res.send({success: true, result});
-})
+
+       //update for admin user
+        app.put('/user/admin/:email', async(req, res)=>{
+          const email = req.params.email;
+          const filter = {email:email};
+          const updateDoc ={
+            $set: {role: 'admin'},
+          }
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        })
+     
+        //update/insert user
+       app.put('/user/:email', async(req, res)=>{
+        const email = req.params.email;
+        const user = req.body;
+        const filter = {email:email};
+        const options = {upsert: true};
+        const updateDoc ={
+          $set: user,
+        }
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+      // const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res.send(result);
+        // res.send(result, token);
+      })  
+    // from db to server to UI 
+      app.get('/service', async(req, res) =>{
+          const query ={};
+          const cursor = serviceCollection.find(query);
+          const services =await cursor.toArray();
+          res.send(services);
+      })
+   //from db to server to ui Dashboard
+    app.get('/booking', async(req, res)=>{
+      const buyer = req.query.buyer;
+      const query = {buyer: buyer};
+      const bookings =await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
+
+  //from db(user collection) to server to ui Dashboard
+  app.get('/user', async(req, res)=>{
+    const users = await userCollection.find().toArray();
+    res.send(users);
+  })
+
+
+
+
+    //from modal to db
+    app.post('/booking', async(req, res)=>{
+      const booking = req.body;
+      const result =await bookingCollection.insertOne(booking);
+      return  res.send({success: true, result});
+    })
 
 
     }
